4a7bbf33e274d3810db7a5d82a26c89de8f864ce
Winston Chang <winston@stdout.org>
2023-12-08 09:53:05 -0600

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "Hello program";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems =
+        [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
+
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs allSystems (system:
+          f rec {
+            pkgs = import nixpkgs { inherit system; };
+            inherit system;
+          });
+
+    in {
+      packages = forEachSupportedSystem ({ pkgs, system, ... }: {
+
+        default = pkgs.stdenv.mkDerivation {
+          name = "hello";
+
+          src = self;
+
+          buildInputs = with pkgs; [ clang ];
+
+          buildPhase = ''
+            cc -o hello hello.c
+          '';
+
+          # The store path to "which" is baked into src/library/base/R/unix/system.unix.R,
+          # but Nix cannot detect it as a run-time dependency because the installed file
+          # is compiled and compressed, which hides the store path.
+          installPhase = ''
+            mkdir $out
+            cp hello $out
+          '';
+
+        };
+      });
+
+      apps = forEachSupportedSystem ({ system, ... }: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/hello";
+        };
+      });
+
+      devShells = forEachSupportedSystem ({ pkgs, system, ... }: {
+        default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+
+          packages = with pkgs; [ git ];
+
+          shellHook = ''
+            echo "Hello world shell"
+          '';
+        };
+      });
+    };
+}
